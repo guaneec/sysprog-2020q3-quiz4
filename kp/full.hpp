0dc61c0e254b82518dac8dc5ba15cca81e34d357
Dan <guaneec@gmail.com>
2020-10-07 20:24:26 +0800

    Update full version

--- kp/full.hpp
+++ kp/full.hpp
@@ -16,31 +16,27 @@ TreeAncestor *treeAncestorCreate(int n, int *parent, int parentSize)
     TreeAncestor *obj = (TreeAncestor *) malloc(sizeof(TreeAncestor));
     obj->n = n;
 
-    int max_level = 32 - __builtin_clz(n) + 1;
+    int max_level = obj->max_level = 32 - __builtin_clz(n);
     obj->parent = (uint16_t *) malloc(n * max_level * sizeof(uint16_t));
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < max_level; j++)
-            obj->parent[i * max_level + j] = NEG1;
-    }
-    for (int i = 0; i < parentSize; i++)
-        obj->parent[i * max_level] = parent[i];
 
-    for (int j = 1;; j++) {
+    for (int i = 0; i < parentSize; i++)
+        obj->parent[i] = parent[i];
+    for (int i = n; i < n * max_level; ++i)
+        obj->parent[i] = NEG1;
+
+    for (int j = 1; j < max_level; j++) {
         int quit = 1;
         for (int i = 0; i < parentSize; i++) {
-            obj->parent[i * max_level + j] =
-                obj->parent[i * max_level + j - 1] == NEG1
+            obj->parent[i + n * j] =
+                obj->parent[i + n * (j - 1)] == NEG1
                     ? NEG1
-                    : obj->parent[(obj->parent[i * max_level + j - 1]) *
-                                      max_level +
-                                  j - 1];
-            if (obj->parent[i * max_level + j] != NEG1)
+                    : obj->parent[obj->parent[i + n * (j - 1)] + n * (j - 1)];
+            if (obj->parent[i + n * j] != NEG1)
                 quit = 0;
         }
         if (quit)
             break;
     }
-    obj->max_level = max_level - 1;
     return obj;
 }
 
@@ -50,7 +46,7 @@ int treeAncestorGetKthAncestor(TreeAncestor *obj, int node, int k)
     while (k && node != NEG1) {
         int i = __builtin_ctzl(k);
         k ^= k & -k;
-        node = obj->parent[node * max_level + i];
+        node = obj->parent[node + obj->n * i];
     }
     return node == NEG1 ? -1 : node;
 }
